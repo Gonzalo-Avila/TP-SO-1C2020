4938dc8e9ca13b5864e64e02df92849b7d960570
Elias Reuter <eliasreuterbsas@gmail.com>
2020-07-15 15:58:54 -0300

    Script simplificado

--- SetearEntorno.sh
+++ SetearEntorno.sh
@@ -1,39 +1,25 @@
 pathTP='/home/utnso'
 
 echo "######################## TP DELIBIRD - Ripped Dinos ##########################"
-echo "Clonando repo de commons..."
-sudo mkdir "${pathTP}/so-commons-library/"
-sudo git clone https://github.com/sisoputnfrba/so-commons-library "${pathTP}/so-commons-library/"
-
-echo "Instalando commons..."
-cd  "${pathTP}/so-commons-library/"
-sudo make clean
-sudo make install
-
 echo "Seteando variable de entorno"
-sudo echo "export LD_LIBRARY_PATH=/home/utnso/tp-2020-1c-Ripped-Dinos/TP-Delibird_SharedLib/Debug/" >> ~/.bashrc
+export LD_LIBRARY_PATH=/home/utnso/tp-2020-1c-Ripped-Dinos/TP-Delibird_SharedLib/Debug/
 
 echo "Compilando Shared Library..."
 cd "${pathTP}/tp-2020-1c-Ripped-Dinos/TP-Delibird_SharedLib/Debug/"
-sudo make clean
-sudo make all
+make all
 
 echo "Compilando broker..."
 cd "${pathTP}/tp-2020-1c-Ripped-Dinos/TP-Delibird_Broker/Debug/"
-sudo make clean
-sudo make all
+make all
 
 echo "Compilando team..."
 cd "${pathTP}/tp-2020-1c-Ripped-Dinos/TP-Delibird_Team/Debug/"
-sudo make clean
-sudo make all
+make all
 
 echo "Compilando gamecard..."
 cd "${pathTP}/tp-2020-1c-Ripped-Dinos/TP-Delibird_Game-Card/Debug/"
-sudo make clean
-sudo make all
+make all
 
 echo "Compilando gameboy..."
 cd "${pathTP}/tp-2020-1c-Ripped-Dinos/TP-Delibird_Game-Boy/Debug/"
-sudo make clean
-sudo make all
+make all
