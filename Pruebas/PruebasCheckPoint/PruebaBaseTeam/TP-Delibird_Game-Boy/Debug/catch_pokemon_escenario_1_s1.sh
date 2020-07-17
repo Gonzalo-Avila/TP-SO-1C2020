c9d321821f18de9d86e5919e9c287de6006aec64
Ramiro-luengo <ramirobmds@gmail.com>
2020-07-17 18:29:27 -0300

    Remuevo logs in innecesarios

--- Pruebas/PruebasCheckPoint/PruebaBaseTeam/TP-Delibird_Game-Boy/Debug/catch_pokemon_escenario_1_s1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./TP-Delibird_Game-Boy TEAM APPEARED_POKEMON Pikachu 1 1
-./TP-Delibird_Game-Boy TEAM APPEARED_POKEMON Squirtle 9 7
-./TP-Delibird_Game-Boy TEAM APPEARED_POKEMON Onix 2 2
--- Pruebas/PruebasFinales/PruebaBaseFileSystem/TP-Delibird_Game-Boy/Debug/catch_charmander.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./TP-Delibird_Game-Boy GAMECARD CATCH_POKEMON Charmander 413 17 1
